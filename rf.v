a4f3384220fb0410b538de402682200bb9e48a2e
avargel <61161389+avargel@users.noreply.github.com>
2020-02-18 02:40:49 +0800

    Update rf.v

--- rf.v
+++ rf.v
@@ -1,1 +1,21 @@
 `timescale 1ns/1ps
+
+module register(
+    input clk,
+    input nrst,
+    input [4:0] rd_addrA,
+    input [4:0] rd_addrB,
+    output [31:0] rd_dataA,
+    output [31:0] rd_dataB,
+    input wr_en,
+    input [4:0] wr_addr,
+    input [31:0] wr_data
+    );
+
+    reg [31:0] int_data [31:0];
+
+    always@(*) begin
+        
+    end
+
+endmodule
